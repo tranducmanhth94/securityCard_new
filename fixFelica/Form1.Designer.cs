f9022538fe37686d6b070f8e12553ba3f619c453
tranducmanhth94 <tranducmanhth94@gmail.com>
2021-12-13 19:22:28 +0900

    first commit

---
+++ fixFelica/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace fixFelica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ID_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ID_textBox
+            // 
+            this.ID_textBox.Location = new System.Drawing.Point(209, 138);
+            this.ID_textBox.Name = "ID_textBox";
+            this.ID_textBox.Size = new System.Drawing.Size(270, 22);
+            this.ID_textBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "id";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(209, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(189, 49);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ID_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ID_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ fixFelica/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FelicaLib;
+
+namespace fixFelica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            /*   try
+               {
+                   using (Felica f = new Felica())
+                   {
+                       readSuica(f);
+                   }
+               }
+               catch (Exception ex)
+               {
+                   *//*Console.WriteLine(ex.Message);*//*
+                   MessageBox.Show(ex.Message);
+               }*/
+
+            string mozi = "";
+            int index = 0;
+            using (Felica f = new Felica())
+            {
+                //ポートの指定
+                f.Polling(0xFFFF);
+                //block00を読み込む
+                byte[] Block00 = f.ReadWithoutEncryption(Service_suica.SERVICE_readonly, 0x83);
+
+                Block00 = Encoding.Default.GetBytes("abc123");
+                Console.WriteLine("byte array is:" + String.Join("", Block00));
+
+
+                mozi = System.Text.Encoding.UTF8.GetString(Block00);
+
+                Console.WriteLine("the string is" + mozi);
+                
+
+            }
+
+        }
+        public struct Service_suica
+        {
+            public const int SERVICE_SUICA_INOUT = 0x108f;
+            public const int SERVICE_SUICA_HISTORY = 0x090f;
+            public const int SERVICE_readonly = 0x000B;
+        }
+
+ /*       public static void Main()
+        {
+            var mc = new Form1();
+          try
+            {
+                using (Felica f = new Felica())
+                {
+                    mc.readSuica(f);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }*/
+
+        /*private int readSuica(Felica f)
+        {
+            f.Polling((int)SystemCode.Pasmo);
+            for(int i = 0; ;i++)
+            {
+                byte[] data;
+                data = f.ReadWithoutEncryption(Service_suica.SERVICE_SUICA_INOUT, i);
+                if (data == null) 
+                    break;
+                suica_dump_history(data);
+            }
+            f.Dispose();
+            return 0;
+        }*/
+       /* public void suica_dump_history(byte[] data)
+        {
+            DataSet dataset = new DataSet();
+            DataTable dt = new DataTable("dataGridView");
+            int ctype, proc, date, time, balance, seq, region;
+            int in_line, in_sta, out_line, out_sta;
+            int yy, mm, dd;
+            // add cloumn
+            dt.Columns.Add("端末類");
+            dt.Columns.Add("処理");
+            dt.Columns.Add("日付");
+            dt.Columns.Add("入");
+            dt.Columns.Add("残高");
+            dt.Columns.Add("連番");
+            dataset.Tables.Add(dt);
+            DataRow datarow = dataset.Tables["dataGridView"].NewRow();
+
+            ctype = data[0];//端末類
+            proc = data[1]; //処理
+            date =  (data[4] << 8) + data[5];// 日付
+            balance = (data[10] << 8) + data[11];// 残高
+            balance = ((balance) >> 8) & 0xff | ((balance) << 8) & 0xff00;
+            seq = (data[12] << 24) + (data[13] << 16) + (data[14] << 8) + data[15];
+            region = seq & 0xff;
+            seq >>= 8;
+
+            out_line = -1;
+            out_sta = -1;
+            time = -1;
+
+            switch (ctype)
+            {
+                case 0xC7:  // 物販
+                case 0xC8:  // 自販機  
+                    time = (data[6] << 8) + data[7];
+                    in_line = data[8];
+                    break;
+                case 0x05:  // 車載機
+                    in_line = (data[6] << 8) + data[7];
+                    in_sta = (data[8] << 8) + data[9];
+                    break;
+
+                default:
+                    in_line = data[6];
+                    in_sta = data[7];
+                    out_line = data[8];
+                    out_sta = data[9];
+                    break;
+            }
+            datarow["端末類"] = ctype;  // check 
+            datarow["処理"] = proc; // check 
+
+            // date
+            yy = date >> 9;
+            mm = (date >> 5) & 0xf;
+            dd = date & 0x1f;
+            datarow["Date"] = yy.ToString() + "/" + mm.ToString() + "/" + dd.ToString() + " ";
+
+            // time
+            if(time > 0)
+            {
+                int hh = time >> 11;
+                int min = (time >> 5) & 0x3f;
+
+                datarow["日付"] += hh.ToString() + ":" + min.ToString();
+            }
+
+            datarow["入"] = in_line.ToString("X") + "/" *//*+ in_sta.ToString("X")*//*;
+            if(out_line != -1)
+            {
+                datarow["入"] = out_line.ToString("X") + "/" + out_sta.ToString("X");
+            }
+            datarow["残高"] = balance.ToString();
+            datarow["連番"] = seq.ToString();
+
+        }
+        private string consoleType(int ctype)
+        {
+            switch (ctype)
+            {
+                case 0x03: return "清算機";
+                case 0x05: return "車載端末";
+                case 0x08: return "券売機";
+                case 0x12: return "券売機";
+                case 0x16: return "改札機";
+                case 0x17: return "簡易改札機";
+                case 0x18: return "窓口端末";
+                case 0x1a: return "改札端末";
+                case 0x1b: return "携帯電話";
+                case 0x1c: return "乗継清算機";
+                case 0x1d: return "連絡改札機";
+                case 0xc7: return "物販";
+                case 0xc8: return "自販機";
+            }
+            return "???";
+        }
+
+        private string procType(int proc)
+        {
+            switch (proc)
+            {
+                case 0x01: return "運賃支払";
+                case 0x02: return "チャージ";
+                case 0x03: return "券購";
+                case 0x04: return "清算";
+                case 0x07: return "新規";
+                case 0x0d: return "バス";
+                case 0x0f: return "バス";
+                case 0x14: return "オートチャージ";
+                case 0x46: return "物販";
+                case 0x49: return "入金";
+                case 0xc6: return "物販(現金併用)";
+            }
+            return "???";
+        }*/
+    }
+}
